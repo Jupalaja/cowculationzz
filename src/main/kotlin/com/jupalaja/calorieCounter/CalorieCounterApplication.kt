1ccd66b2c57d3d944f5a612d9d4d0e3b266d2764
jupalaja <jupalaja@gmail.com>
2025-07-28 23:28:16 -0500

    :tada: initial commit for caloriecounter project

---
+++ src/main/kotlin/com/jupalaja/calorieCounter/CalorieCounterApplication.kt
@@ -0,0 +1,11 @@
+package com.jupalaja.calorieCounter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalorieCounterApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalorieCounterApplication>(*args)
+}
---
+++ src/test/kotlin/com/jupalaja/calorieCounter/CalorieCounterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jupalaja.calorieCounter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalorieCounterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
